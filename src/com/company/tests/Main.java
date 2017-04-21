6f2faa8f52db566d8d8aab5fdf7584cfe5bfd33f
AGoryuchkina <AVGoryuchkina@ya.ru>
2017-04-21 19:01:02 +0300

    Fix problem with Nullpointer Exception in index loop. It could not get method from null index, which appears from the ROLES groups (wothout indexes)

--- src/com/company/tests/Main.java
+++ src/com/company/tests/Main.java
@@ -1,8 +1,6 @@
 package com.company.tests;
 
-import java.lang.reflect.Array;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
+import java.io.File;
 import java.util.List;
 
 import static com.company.tests.ExcelReader.readExcel;
@@ -10,13 +8,26 @@ import static com.company.tests.ExcelReader.readExcel;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
         GetDepartmentInfo.getDepartment(); //заполняем листы департаментов и их индексов
-        List<UserProfile> users = readExcel();//get data from excel and set it to UserProfile object
-        for (UserProfile user : users) {
-            UserProfileCheck checkUser = new UserProfileCheck(user);
-            checkUser.check();
+
+        File folder = new File("C:\\Users\\AGoryuchkina\\Desktop\\fileFolder1");
+        File[] folderEntries = folder.listFiles();
+        for (File entry : folderEntries) {
+            System.out.println(entry.getName());
+            List<FullUserProfile> users = readExcel(folder + "\\" + entry.getName());//get data from excel and set it to FullUserProfile object
+            for (FullUserProfile user : users) {
+                UserProfileCheck checkUser = new UserProfileCheck(user);
+                checkUser.check();
+            }
+            List<FullUserProfile> fullUsers = UserProfileCheck.fullUsers;
+            ExcelWriter.writeFile(fullUsers, folder + "\\" + entry.getName() + " - result.xlsx");
+            for (FullUserProfile fullUser : fullUsers) {
+                ExcelWriter.writeSQLQueryForOPSResp(fullUser);
+            }
+            users.clear();
+            fullUsers.clear();
+            FullUserProfile.setID(0);
         }
-        List<FullUserProfile> fullUsers = UserProfileCheck.fullUsers;
-        ExcelWriter.writeFile(fullUsers);
     }
 }
